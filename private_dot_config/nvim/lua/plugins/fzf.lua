d714341858049cc8bedab8c4cc852b9e74810b24
Ferran Basora <fcsonline@gmail.com>
2022-06-28 15:30:55 +0000

    Tune fzf

--- private_dot_config/nvim/lua/plugins/fzf.lua
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -4,10 +4,11 @@ require("fzf-lua").setup({
 		async_or_timeout = 3000,
 	},
 	winopts = {
-		height = 0.3,
-		width = 1,
-		row = 1,
-		col = 0,
+    height           = 0.85,            -- window height
+    width            = 0.80,            -- window width
+    row              = 0.35,            -- window row position (0=top, 1=bottom)
+    col              = 0.50,            -- window col position (0=left, 1=right)
+
 		preview = {
 			layout = "horizontal",
 		},
