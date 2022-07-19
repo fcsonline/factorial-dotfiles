f2273bc80e7760b3a16e45e7ab2e63aa56312aa2
Ferran Basora <fcsonline@gmail.com>
2022-07-19 15:20:38 +0000

    Tune dotfiles

--- private_dot_config/bash/alias.bash
+++ private_dot_config/bash/alias.bash
@@ -2,6 +2,14 @@
 
 alias ..='cd ..'         # Go up one directory
 alias ...='cd ../..'     # Go up two directories
+alias cd..='cd ..'
+alias cd...='cd ../..'
+
+# LS
+alias ls='ls --color=auto'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alFh'
 
 # Tree
 if [ ! -x "$(which tree)" ]
@@ -10,18 +18,23 @@ then
 fi
 
 # GIT
-
-# Aliases
 alias gap='git add -p'
-alias gs='git status'
-alias gl='git pull'
-alias gpu='git push'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gco='git checkout'
-alias gg='git grep'
-alias gst='git diff main --compact-summary'
-alias gca='git commit --amend'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gg='git grep -n -C2 --color=always'
+alias gg0='git grep -n -C0 --color=always'
+alias gitroot='cd $(git rev-parse --show-cdup)'
+alias gl='git log'
+alias grm='git rebase master'
+alias gm='git commit -m'
+alias gma='git commit --amend --no-edit'
+alias gpu='git publish'
+alias gpr='git pull --rebase'
+alias gs='git status -s'
+
+# UTILS
+alias remove_colors='sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"'
+alias vim='nvim'
 
 # Devenv
 alias devenv='tmuxinator start -p gitpod/.tmuxinator.yml'
