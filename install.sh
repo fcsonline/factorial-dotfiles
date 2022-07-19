7679c87df114be606203c2f6d3f932b5eda46b25
Ferran Basora <fcsonline@gmail.com>
2022-07-19 19:35:59 +0000

    Install starship

--- install.sh
+++ install.sh
@@ -10,8 +10,6 @@ popd
 
 ~/bin/chezmoi apply
 
-source ~/.bashrc
-
 # Install tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 TMUX_PLUGIN_MANAGER_PATH="${HOME}/.tmux/plugins/" ~/.tmux/plugins/tpm/bin/install_plugins
