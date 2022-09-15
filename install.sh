441a3b4f2097f156599f9d269529f3ed200c797b
Ferran Basora <fcsonline@gmail.com>
2022-09-15 15:47:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ popd
 
 # Install tmux plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-TMUX_PLUGIN_MANAGER_PATH="${HOME}/.tmux/plugins/" ~/.tmux/plugins/tpm/bin/install_plugins &
+TMUX_PLUGIN_MANAGER_PATH="${HOME}/.tmux/plugins/" ~/.tmux/plugins/tpm/bin/install_plugins & disown
 
 # Install packer dependencies
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &
-nvim --headless -c 'TSUpdateSync' &
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' & disown
+nvim --headless -c 'TSUpdateSync' & disown
