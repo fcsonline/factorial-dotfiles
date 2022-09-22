ddecf90177e852a2b594aa8501e67d7e83f8e19b
Ferran Basora <fcsonline@gmail.com>
2022-09-22 15:52:08 +0200

    Update nvim.sh

--- gitpod/nvim.sh
+++ gitpod/nvim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install packer dependencies
-timeout 60 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerClean' -c 'PackerSync' -c 'TSUpdateSync'
+timeout 60 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync' -c 'silent TSUpdateSync'
