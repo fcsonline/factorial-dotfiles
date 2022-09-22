2422c74891eff9e54a3e7276f023ddf55fbbf540
Ferran Basora <fcsonline@gmail.com>
2022-09-22 16:05:33 +0200

    Update settings.lua

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -12,7 +12,7 @@ cmd("set synmaxcol=200") -- Limits the syntax highlighting
 vim.o.termguicolors = true
 vim.o.background = "dark"
 vim.g.syntax = true
-cmd([[colorscheme catppuccin]])
+-- cmd([[colorscheme catppuccin]])
 
 -- SETTINGS
 opt.backup = false -- creates a backup file
