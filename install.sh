65df4abd12a4ee9894766cce1c94f0e980a5dc2e
Ferran Basora <fcsonline@gmail.com>
2023-09-21 12:02:15 +0200

    Move starship installation

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ popd
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 TMUX_PLUGIN_MANAGER_PATH="${HOME}/.tmux/plugins/" ~/.tmux/plugins/tpm/bin/install_plugins
 
+if ! [ -x "$(command -v starship)" ]; then
+  curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir ${HOME}/.local/bin >/dev/null 2>&1
+fi
+
 # Install packer dependencies
 # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
 # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync' # Yep, twice
